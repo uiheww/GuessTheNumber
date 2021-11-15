fbe5e337b2c74f068e245053e45e4daa000b1bee
lankimhe <92359757+lankimhe@users.noreply.github.com>
2021-11-15 17:34:27 +1000

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main{
+public class main{
     public static void main(String [] args) {
         //region operations
         System.out.println("Введите операцию");
